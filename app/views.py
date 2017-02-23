a65576c4328b0456be1c554d4059da7baf9f22d5
Xander <alexander.fehsenfeld@coloradocollege.edu>
2017-02-23 12:47:54 -0800

    Added tests and and jsonp capability

--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@
 import netCDF4 as nc
 from app import app
 from flask import send_from_directory
-import os
 from flask import Response
+from flask import request
+import os
 import json
 
 
@@ -17,10 +18,37 @@ handlers are written as python functions
 
 dataFN = 'app/static/ocean_his_0002.nc'
 
-#Open a .nc dataset and serve the data
+#serve the data in json
 @app.route('/')
 @app.route('/index')
 def index():
+    
+    output = getData()
+       
+    return Response(json.dumps(output), mimetype='application/json')
+
+#serve data in jsonp with provided callback
+@app.route('/jsonp')
+def jsonp():
+       
+    output = getData()
+    
+    callback = request.args.get('callback')
+       
+    return Response( callback + "(" + json.dumps(output) + ")", mimetype='application/json')
+    
+
+#convert a 2 dimensional numpy array to a python array
+#empties are converted to null
+def numpyToArray( numpy ):
+    rows = len( numpy[0] )
+    columns =  len( numpy )
+    numpy = numpy.reshape( columns, rows )
+    
+    return numpy.tolist()
+
+#open a .nc file and collect data
+def getData():
     ds = nc.Dataset( dataFN )
     salt = ds.variables['salt'][0, 0, :, :].squeeze()
     lonp = ds.variables['lon_psi'][:]
@@ -31,16 +59,8 @@ def index():
     output['lonp'] = numpyToArray(lonp)
     output['latp'] = numpyToArray(latp)
     
-    return Response(json.dumps(output), mimetype='application/json')
-
-#convert a 2 dimensional numpy array to a python array
-#empties are converted to null
-def numpyToArray( numpy ):
-    rows = len( numpy[0] )
-    columns =  len( numpy )
-    numpy = numpy.reshape(columns, rows)
+    return output
     
-    return numpy.tolist()
 
 
 
