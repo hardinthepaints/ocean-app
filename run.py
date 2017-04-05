bd8419fe2c9e804a1d04ee8347b6b9388c57b0e1
Xander <alexander.fehsenfeld@coloradocollege.edu>
2017-04-05 11:31:50 -0700

    Removed irrelevant parameter.

--- run.py
+++ run.py
@@ -4,14 +4,11 @@ import sys
 
 from app import app
 
-#import the app variable from our app package
-#invoke its run method to start server
-#app.run( debug=True )
-
-
-
-if ( "-d" in sys.argv ): app.run( debug=True, extra_files=["tests/plotly/javascript_test.html"] )
-else: app.run( debug=False )
+#run in debug mode if in args
+if ( "-d" in sys.argv ):
+    app.run( debug=True, extra_files=["tests/plotly/javascript_test.html"] )
+else:
+    app.run( debug=False )
 
 
 
