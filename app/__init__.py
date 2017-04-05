1c2429136eefa457b1b07683da0f55c72b7cff25
Xander <alexander.fehsenfeld@coloradocollege.edu>
2017-04-05 11:26:19 -0700

    Incorporated in the functions in db_functions and auth_functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 import os
+from flask import Flask, g
 
-from flask import Flask
+#allow cross origin requests
 from flask_cors import CORS, cross_origin
-
 
 #creates the application object of class 'Flask'
 #according to documentation it is necesary to define the root path
 app = Flask(__name__, root_path = os.getcwd() )
+
 cors = CORS( app )
 
 #imports 'views' --at end of file to avoid circular refs
-from app import views
+from app import views, db_functions
 
 
