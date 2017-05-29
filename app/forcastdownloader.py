e7ebe007da34090e0cb7ae7c0b94b90423d8865f
Xander <alexander.fehsenfeld@coloradocollege.edu>
2017-05-29 12:21:19 -0700

    Fixed filenotfound error.

--- app/forcastdownloader.py
+++ app/forcastdownloader.py
@@ -59,8 +59,9 @@ def ensure_dir(f):
         
 # download a file     
 def downloadFile( url ):
-     
-    final_filename = 'ncFiles/{}/{}'.format( url[1], url[2])
+    
+    abs_filepath = getCurrentDirectory() + 'ncFiles/{}/{}'
+    final_filename = abs_filepath.format( url[1], url[2])
     local_filename = final_filename.replace('.nc', ".temp")
     
     #convert tuple to a string to build the url
